6cf5e47fc67d2b544b0702d17ff62750eed56915
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-18 14:33:46 +0530

    feat: added translation functionality

---
+++ app.js
@@ -0,0 +1,27 @@
+// selecting and storing elements
+var btnTranslate = document.querySelector("#btn-translate")
+var inputTextarea = document.querySelector("#input-textarea")
+var translationOutput = document.querySelector("#translation-output")
+
+// minions translation api url
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+// function to add the user input text to the URL
+function appendURL(value){
+    var encodedValue = encodeURIComponent(value)
+    var append = url + "?text=" + encodedValue
+    return append
+}
+
+// chaining the click event listener on the translate button
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    var inputText = inputTextarea.value //input
+
+    //processing
+    fetch(appendURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        translationOutput.innerText = json.contents.translated //output
+    })
+    .catch(err => console.log(err)) //error handling
+})
